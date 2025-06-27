f5fb2d2adc35829905e2df3cc82fbffe80e46505
Itshamzaoy <isardarhamzaofficial@gmail.com>
2025-06-27 18:56:09 +0500

    Update config.js

--- config.js
+++ config.js
@@ -78,8 +78,6 @@ READ_CMD: process.env.READ_CMD || "false",
 // mark commands as read 
 DEV: process.env.DEV || "50934960331",
 // your whatsapp number        
-ANTI_VV: process.env.ANTI_VV || "true",
-// anti view once 
 AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
 // auto recording status 
 };
